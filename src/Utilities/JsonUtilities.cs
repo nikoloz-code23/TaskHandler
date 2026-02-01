68197acbcb26b2e23f674e5cc5fab1d3059c4ad7
nikoloz-code23 <literallynickk@gmail.com>
2026-02-02 03:30:55 +0400

    Added an Update function. Still a WIP, need to figure out an elegant solution to handle Id's of the tasks.

--- src/Utilities/JsonUtilities.cs
+++ src/Utilities/JsonUtilities.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
@@ -13,8 +14,9 @@ public static class JsonUtilities
         WriteIndented = true
     };
 
-    public static void AddElementToArray<T>(string filePath, T obj)
+    public static void AddElementToArray<T>(string filePath, object obj)
     {
+        // TODO: Work on having functionality for incrementing an ID.
         if (!File.Exists(filePath))
         {
             FileUtilities.CreateFile(filePath, "[]");
@@ -26,10 +28,10 @@ public static class JsonUtilities
         {
             jsonNode = JsonNode.Parse(File.ReadAllText(filePath));
         }
-        catch (JsonException)
+        catch (JsonException e)
         {
-            Console.WriteLine("The JSON is invalid.");
-            return;
+            Console.WriteLine($"The JSON is invalid: {e}");
+            throw;
         }
         
         JsonArray jsonArray;
@@ -38,10 +40,10 @@ public static class JsonUtilities
         {
             jsonArray = jsonNode!.AsArray();   
         }
-        catch (InvalidOperationException)
+        catch (InvalidOperationException e)
         {
-            Console.WriteLine("The JSON does not contain an array.");
-            return;
+            Console.WriteLine($"The JSON does not contain an array: {e}");
+            throw;
         }
         
         string serializedObj = JsonSerializer.Serialize(obj);
@@ -49,5 +51,70 @@ public static class JsonUtilities
 
         string jsonString = jsonArray!.ToJsonString(JsonOptions);
         File.WriteAllText(filePath, jsonString);
+
+        Console.WriteLine("New element added succesfully!");
+    }
+
+    public static void UpdateElementInArray<T>(string filePath, object valueToSearch, T newData, string fieldName)
+    {
+        string? fileData = null;
+
+        try
+        {
+            fileData = File.ReadAllText(filePath);
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine(e);
+        }
+
+        List<T>? elementsInJson;                
+        
+        if (fileData == null)
+            throw new ArgumentNullException("Can't access file data");
+
+        try
+        {
+            elementsInJson = JsonSerializer.Deserialize<List<T>>(fileData);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        if (elementsInJson == null || elementsInJson.Count == 0)
+        {
+            Console.WriteLine("There are no elements to update.");
+            return;
+        }
+
+        for(int i = elementsInJson.Count - 1; i >= 0; i--)
+        {
+            object? element = elementsInJson[i];
+
+            if (element == null) continue;
+
+            Type type = element.GetType();
+            PropertyInfo? field = type.GetProperty(fieldName);
+            if (field == null)
+            {
+                Console.WriteLine("Property doesn't exist! Aborting.");
+                return;
+            }
+            object? value = field.GetValue(element);
+
+            if (value != null && value.Equals(valueToSearch))
+            {
+                //TODO: Make it so it won't overwrite certain properties.
+                elementsInJson[i] = newData;
+    
+                string newJsonString = JsonSerializer.Serialize(elementsInJson, JsonOptions);
+                
+                File.WriteAllText(filePath, newJsonString);
+                
+                Console.WriteLine($"Element {valueToSearch} updated succesfully!");
+            }
+        }
     }
 }
