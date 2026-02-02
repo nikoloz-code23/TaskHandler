ba90725ee349e137da8144541f9cbf8184c7701b
nikoloz-code23 <literallynickk@gmail.com>
2026-02-03 01:30:17 +0400

    Changed the Id Type of the task object.

--- src/Types/Task.cs
+++ src/Types/Task.cs
@@ -5,7 +5,7 @@ namespace TaskTracker.Types;
 
 public record TodoTask
 {
-    public string? Id { get; set; }
+    public int Id { get; set; }
     public string? Description { get; set; }
     public TaskStatus Status { get; set; }
     public DateTime CreatedAt { get; set; }
