aa83c6b99a0198357fc3902a1a2f9f91d658f6e9
nikoloz-code23 <literallynickk@gmail.com>
2026-02-04 16:24:41 +0400

    Created a new Utility for helping with setting or getting values of objects

---
+++ src/Utilities/ObjectUtilities.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Reflection;
+
+namespace TaskTracker.Utilities;
+
+public static class ObjectUtilities
+{
+    public static object? ReturnValueFromProperty(Type objectType, object obj, string propertyName)
+    {
+        PropertyInfo? property = objectType.GetProperty(propertyName);
+        if(property == null)
+        {
+            Console.WriteLine($"{propertyName} does not exist.");
+            return null;
+        }
+        return property.GetValue(obj);
+    }
+
+    public static void SetValueInProperty(Type objectType, object obj, string propertyName, object newValue)
+    {
+        PropertyInfo? property = objectType.GetProperty(propertyName);
+        if (property == null)
+        {
+            Console.WriteLine($"{propertyName} does not exist! Aborting.");
+            return;
+        }
+        
+        property.SetValue(obj, newValue);
+    } 
+}
