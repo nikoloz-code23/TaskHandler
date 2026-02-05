2e290a1946748c1800a166b92e7a87f4ea3e253b
nikoloz-code23 <literallynickk@gmail.com>
2026-02-05 23:30:11 +0400

    Edited App.cs to finally be able to parse data and commands.

--- src/App.cs
+++ src/App.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using TaskTracker.Handlers;
+using TaskTracker.Commands;
+using TaskTracker.Utilities;
+using TaskTracker.Factory;
 
 namespace TaskTracker.Application;
 
 public class App
 {
+    public string FilePath { get; set; } = "./todo.json";
     public string AppName { get; set; }
     public char AppNameSuffix { get; set; }
 
@@ -34,6 +38,12 @@ public class App
         InputHandler inputHandler = new InputHandler();
         inputHandler.TextMark = textMark;
 
+        JsonUtilities jsonUtilities = new();
+        jsonUtilities.IdPropertyName = "Id";
+        jsonUtilities.UpdatePropertyName = "UpdatedAt";
+
+        TodoTaskFactory factory = new(FilePath, jsonUtilities, "Id");
+
         while(true)
         {
             Console.Write(appPrefix);
@@ -45,10 +55,41 @@ public class App
 
             inputHandler.HandleInput(inputList);
 
-            foreach(var element in inputList)
+            string command = inputList[0].ToLower();
+
+            for(int i = 0; i < inputList.Count; i++)
             {
-                Console.WriteLine(element);
+                Console.WriteLine($"{i}: {inputList[i]}");
+            }
+
+            switch(command)
+            {
+                case "add":
+                    AddCommand addCommand = new(inputList[1], factory);
+                    addCommand.Execute(FilePath, jsonUtilities);
+                break;
+
+                case "update":
+                    UpdateDescriptionCommand updateDescriptionCommand = new(int.Parse(inputList[1]), inputList[2], "Description");
+                    updateDescriptionCommand.Execute(FilePath, jsonUtilities);
+                break;
+
+                case "mark-in-progress":
+                    MarkInProgressCommand markInProgressCommand = new(int.Parse(inputList[1]));
+                    markInProgressCommand.Execute(FilePath, jsonUtilities);
+                break;
+
+                case "mark-done":
+                    MarkDone markDoneCommand = new(int.Parse(inputList[1]));
+                    markDoneCommand.Execute(FilePath, jsonUtilities);
+                break;
+
+                case "delete":
+                    RemoveCommand removeCommand = new(int.Parse(inputList[1]));
+                    removeCommand.Execute(FilePath, jsonUtilities);
+                break;
             }
         }
+        
     }
 }
