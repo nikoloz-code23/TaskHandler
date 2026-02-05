c1e4b6ac0457329ab9c0d4bd3c166f79649aa048
nikoloz-code23 <literallynickk@gmail.com>
2026-02-05 23:27:58 +0400

    Created a Mark Done and Mark In Progress commands.

---
+++ src/Commands/MarkInProgressCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using TaskTracker.Types;
+using TaskTracker.Interfaces;
+using TaskTracker.Utilities;
+using TaskTracker.Enums;
+
+namespace TaskTracker.Commands;
+
+public class MarkInProgressCommand : ICommand {
+    public object? Id { get; set; }
+
+    public MarkInProgressCommand(object id)
+    {
+        Id = id;
+    }
+
+    public void Execute(string filePath, JsonUtilities jsonUtilities)
+    {
+        if (Id == null)
+        {
+            Console.WriteLine("Specify the id of the element to remove.");
+            return;
+        }
+
+        jsonUtilities.UpdateElementInArray<TodoTask>(filePath, Id, TodoTaskStatus.IN_PROGRESS, "Status");
+    }
+}
