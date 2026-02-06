ebe6cc5794e8c5ee2b061a6f0dc90de651593c25
nikoloz-code23 <literallynickk@gmail.com>
2026-02-06 14:26:22 +0400

    Forgot to commit this with the previous commit, but updated the TodoTask to take in a string for CreatedAt and UpdatedAt instead of having pure DateTime. This will make sure the format is consistent.

--- src/Types/Task.cs
+++ src/Types/Task.cs
@@ -8,6 +8,6 @@ public record TodoTask
     public int Id { get; set; }
     public string? Description { get; set; }
     public TodoTaskStatus Status { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    public string? CreatedAt { get; set; }
+    public string? UpdatedAt { get; set; }
 }
